9c00d0c64e0552fb12619d3202eab53b86a83950
Nikos Sarris <nsarris@gmail.com>
2022-03-17 15:42:57 +0200

    WIP

--- src/FluentAuthorization/Policy/Policy.AssertionContext.cs
+++ src/FluentAuthorization/Policy/Policy.AssertionContext.cs
@@ -5,6 +5,9 @@ namespace FluentAuthorization
 {
     public abstract partial class Policy<TUser, TResource, TData>
     {
+        /// <summary>
+        /// A permission assertion context implementation.
+        /// </summary>
         public class AssertionContext : AssertionContextBase
         {
             private readonly IPermission permission;
@@ -18,7 +21,18 @@ namespace FluentAuthorization
             private AssertionFailure BuildFailure(string reason)
                 => new AssertionFailure(User.ToString(), PermissionName, PolicyName, permission.BuildMessage(this), reason);
 
+            /// <summary>
+            /// Produces a Deny result with the specified reason.
+            /// </summary>
+            /// <param name="reason">The reason for the permssion failure.</param>
+            /// <returns>A Deny result with the specified reason.</returns>
             public AssertionResult Deny(string reason = null) => new(BuildFailure(reason));
+
+            /// <summary>
+            /// Produces a Deny result with the specified reasons.
+            /// </summary>
+            /// <param name="reason">The reasons for the permssion failure.</param>
+            /// <returns>A Deny result with the specified reasons.</returns>
             public AssertionResult Deny(IEnumerable<string> reasons) => new(reasons.Select(BuildFailure));
         }
     }
--- src/FluentAuthorization/Policy/Policy.AssertionContext_TState.cs
+++ src/FluentAuthorization/Policy/Policy.AssertionContext_TState.cs
@@ -5,6 +5,9 @@ namespace FluentAuthorization
 {
     public abstract partial class Policy<TUser, TResource, TData>
     {
+        /// <summary>
+        /// A state handling permission assertion context implementation.
+        /// </summary>
         public class AssertionContext<TState> : AssertionContextBase
         {
             private readonly IPermission<TState> permission;
@@ -21,7 +24,18 @@ namespace FluentAuthorization
             private AssertionFailure BuildFailure(string reason)
                 => new AssertionFailure(User.ToString(), PermissionName, PolicyName, permission.BuildMessage(this), reason);
 
+            /// <summary>
+            /// Produces a Deny result with the specified reason.
+            /// </summary>
+            /// <param name="reason">The reason for the permssion failure.</param>
+            /// <returns>A Deny result with the specified reason.</returns>
             public AssertionResult Deny(string reason = null) => new(BuildFailure(reason));
+
+            /// <summary>
+            /// Produces a Deny result with the specified reasons.
+            /// </summary>
+            /// <param name="reason">The reasons for the permssion failure.</param>
+            /// <returns>A Deny result with the specified reasons.</returns>
             public AssertionResult Deny(IEnumerable<string> reasons) => new(reasons.Select(BuildFailure));
         }
     }
